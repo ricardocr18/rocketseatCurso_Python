b3cc9789f9029e1a48e9686e2d73a5f8e1be31a6
ricardocrNatura <ricardoribeiro.compasso@natura.net>
2026-04-11 00:40:16 -0300

    criando projeto Desafio Nivel3

---
+++ nivel3/desafioPratico/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def on_connect():
+    sid = request.sid
+    print(f'Client connected: {sid}')
+
+@socketio.on('message')
+def handle_message(msg):
+    emit('message', msg, broadcast=True)
+    print(f'Message: {msg}')
+
+@socketio.on('disconnect')
+def on_disconnect():
+    sid = request.sid
+    print(f'Client disconnected: {sid}')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
