e7b4aef229f2adb8a35f0b367450b9e754c79c90
ricardocrNatura <ricardoribeiro.compasso@natura.net>
2026-04-16 23:04:30 -0300

    Testes com Dependências

--- nivel4/atividadeProposta1/src/calculators/calculator_2_test.py
+++ nivel4/atividadeProposta1/src/calculators/calculator_2_test.py
@@ -1,13 +1,17 @@
 from .calculator_2 import Calculator2
-from typing import Dict
+from typing import Dict, List
 from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.drivers_handler_interface import DriverHandlerInterface
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
         self.json = body
 
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_derivation(self, number: List[float]) -> float:
+        return 3 # aqui estamos retornando um valor fixo para o desvio padrão, para que possamos testar a lógica do cálculo sem depender do resultado real do desvio padrão.
 
-def test_calculate():
+def test_calculate_integration():
     mock_request = MockRequest({
         "numbers": [2.12, 4.62, 1.32]
         })
@@ -24,5 +28,22 @@ def test_calculate():
         "data": {
             "Calculator": 2,
             "result": 0.08
+            }
+        }
+    
+def test_calculate():
+    mock_request = MockRequest({
+        "numbers": [2.12, 4.62, 1.32]
+        })
+    
+    driver = MockDriverHandler()
+    calculator_2 = Calculator2(driver)
+    format_response = calculator_2.calculate(mock_request)    
+
+    assert isinstance(format_response, dict)
+    assert format_response == {
+        "data": {
+            "Calculator": 2,
+            "result": 0.33
             }
         }
